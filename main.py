6f28f93eb2229a9d4cc085abaa8c98f4a0f71a2f
timcheahyi <timcheah1998@gmail.com>
2023-12-08 17:31:08 +0800

    added parser

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"tim" : {"age" : 25, "gender":"male"},
-         "wach" : {"age" : 23, "gender":"female"}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="name required", required = True)
+video_put_args.add_argument("views", type=int, help="views required")
+video_put_args.add_argument("likes", type=int, help="likes required")
 
-class homePage(Resource):
-    def get(self):
-        return{"data":"home page"}
+videos = {}
 
-class name(Resource):
-    def get(self, name):
-        return names[name]
+def checkId(video_id):
+    if video_id not in videos:
+        abort("not in database")
 
-api.add_resource(homePage, "/")
-api.add_resource(name, "/<string:name>")
+class video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+    
+    def post(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return ("Video posted at " + str(video_id))
+    
+api.add_resource(video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://localhost:5000/"
 
-res = requests.post(BASE)
+res = requests.post(BASE + "video/1", {"likes":10})
 print(res.json())
