000747a8f1af37fdee9f5e3e7f6066424b396d2c
timcheahyi <timcheah1998@gmail.com>
2023-12-08 18:01:28 +0800

    added delete

--- main.py
+++ main.py
@@ -11,18 +11,29 @@ video_put_args.add_argument("likes", type=int, help="likes required")
 
 videos = {}
 
-def checkId(video_id):
+def abortIfNoExist(video_id):
     if video_id not in videos:
-        abort("not in database")
+        abort(404, message = "not in database")
+        
+def abortIfExist(video_id):
+    if video_id in videos:
+        abort(409, message = "already in database")
 
 class video(Resource):
     def get(self, video_id):
+        abortIfNoExist(video_id)
         return videos[video_id]
     
     def post(self, video_id):
+        abortIfExist(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
-        return ("Video posted at " + str(video_id))
+        return ("Video posted at http://localhost:5000/video/" + str(video_id))
+    
+    def delete(self, video_id):
+        abortIfNoExist(video_id)
+        del videos[video_id]
+        return ("Video deleted at http://localhost:5000/video/" + str(video_id))
     
 api.add_resource(video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,5 +2,12 @@ import requests
 
 BASE = "http://localhost:5000/"
 
-res = requests.post(BASE + "video/1", {"likes":10})
+# res = requests.post(BASE + "video/1/", {
+#     "name" : "video1",
+#     "views": 12,
+#     "likes": 2
+# })
+
+res = requests.get(BASE + "video/1")
+
 print(res.json())
