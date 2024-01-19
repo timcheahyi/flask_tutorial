19eff1ce692711cca92136ecec9e4c74fab77a78
timcheahyi <timcheah1998@gmail.com>
2024-01-19 20:42:22 +0800

    main is messed up, just look at notes

---
+++ flaskNotes.md
@@ -0,0 +1,9 @@
+download requirements.txt
+pip install -r requirements.txt
+pip install flask flask_sqlalchemy flask_login flask_bcrypt flask_wtf wtforms email_validator
+
+<<<<<<<<<<main.py>>>>>>>>>>
+remember to change debug to false in prod env
+when returning data, make sure its json serializable (python dics)
+
+flask with htlm frontend: https://github.com/arpanneupane19/Python-Flask-Authentication-Tutorial
--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import wtforms
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 
 # Creating a Flask app instance
 app = Flask(__name__)
-# Initializing a RESTful API with the Flask app
+# Initializing a RESTful API with the Flask apps
 api = Api(app)
 # Configuring the SQLite database URI for SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -27,7 +31,24 @@ class videoModel(db.Model):
         return f"Video(name ={self.name}, views = {self.views}, likes = {self.likes})"
 
 
-# Setting up argument parsing for PUT requests
+class User(db.Model, UserMixin):
+    # Defining columns for the model
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+
+# Setting up argument parsing for requests
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument(
     "name", type=str, help="Name of the video is required", required=True)
@@ -65,19 +86,40 @@ class video(Resource):
         return (video, 201)
 
     # DELETE method for deleting a video
-        def delete(self, video_id):
+    @marshal_with(resource_fields)
+    def delete(self, video_id):
         result = videoModel.query.filter_by(id=video_id).first()
         if not result:
             abort(404, message="Video with ID {} doesn't exist".format(video_id))
 
         db.session.delete(result)
         db.session.commit()
-        
+
         return {"message": "Video deleted", "video_id": video_id}, 200
+
+
+class videos(Resource):
+    # GET method for retrieving all videos
+    @marshal_with(resource_fields)
+    def get(self):
+        # Query all records from the videoModel table
+        videos = videoModel.query.all()
+        return videos
+
+
+@app.route('/login')
+def login():
+    return "login"
+
+
+@app.route('/register')
+def register():
+    return "register"
 
 
 # Adding the video resource to the API
 api.add_resource(video, "/video/<int:video_id>")
+api.add_resource(videos, "/videos")
 
 # Running the Flask app
 if __name__ == "__main__":
